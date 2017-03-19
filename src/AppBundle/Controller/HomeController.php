d78087d2067523e4b15935024e0b74bec45944d3
ekiscrim <xcstudiosonly@gmail.com>
2017-03-19 14:14:44 +0100

    primer commit

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        //$response = new Response('Hola guapo',Response::HTTP_OK,array('content-type' => 'text/html'));
+        //return $response;
+        return $this->render('home.html.twig');
+    }
+}
